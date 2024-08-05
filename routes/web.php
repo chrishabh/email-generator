8d5365df1b9cf88e189f3be48ebc2870b628216c
chrishabh <ch.rishabh8527@gmail.com>
2024-08-06 01:09:52 +0530

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -37,20 +37,20 @@ Route::get('/single-verification', function () {
 Route::get('/bulk-verification', function () {
     return view('bulk-verification');
 });
-Route::get('/signup', function () {
-    return view('signup');
-});
+// Route::get('/signup', function () {
+//     return view('auth/signup');
+// });
 
-Route::get('/payment', function () {
-    return view('payment');
-});
+// Route::get('/payment', function () {
+//     return view('payment');
+// });
 
-Route::get('/signin', function () {
-    return view('signin');
-});
+// Route::get('/signin', function () {
+//     return view('auth/signin');
+// });
 
-Route::get('/create-order', [PaymentController::class, 'createOrder']);
-Route::post('/handle-payment', [PaymentController::class, 'handlePayment']);
+Route::post('/create-order', [PaymentController::class, 'createOrder'])->name('create.Order');;
+Route::post('/handle-payment', [PaymentController::class, 'handlePayment'])->name('handlePayment');;
 
 
 // Route::post('/generate-email', [EmailController::class, 'generateEmail'])->name('generateEmail');
