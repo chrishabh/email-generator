8a5e5ec5c0bac8fefcceb0601017b9e31cdd2cd1
chrishabh <ch.rishabh8527@gmail.com>
2024-09-16 00:41:52 +0530

    test

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <base href="{{env(APP_URL}}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     @stack('title')
 
