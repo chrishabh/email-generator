8c1f5fb90fe57d3076570724d43d64547b51123f
chrishabh <ch.rishabh8527@gmail.com>
2024-10-01 23:36:22 +0530

    update

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -261,8 +261,8 @@ class ProfileController extends Controller
     public function getOverallCreditsReport()
     {
         // try{
-            $credits                 = UserCredits::join('users', 'users.id', '=','user_id')->withTrashed()->whereNull('users.deleted_at')->get(); 
-            $totalCredits            = $credits->whereNull('user_credits.deleted_at1')->sum('credits'); // Sum of all credits
+            $credits                 = UserCredits::join('users', 'users.id', '=','user_id')->whereNull('users.deleted_at')->get(); 
+            $totalCredits            = $credits->whereNull('user_credits.deleted_at')->sum('credits'); // Sum of all credits
             $usedCredits             = UserCredits::getUsedCredits()??0; // Soft deleted credits
             $availableCredits        = $totalCredits;  
             $creditAvailableOfAdmin  = 0;
