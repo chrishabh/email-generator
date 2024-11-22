37ee76d5b9d0a2a554b75df23a25bb240ac2aaa1
chrishabh <ch.rishabh8527@gmail.com>
2024-11-23 03:26:57 +0530

    Update pagibation

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -52,7 +52,7 @@ class ProfileController extends Controller
     function renderSettingPage(Request $request)
     {
         try {
-            $perPage = 10; // Number of users per page
+            $perPage = 50; // Number of users per page
             $currentPage = $request->input('page', 1); // Get the current page or default to 1
             
             $paginationData = User::getUserDetailsWithRemainingCredits($perPage, $currentPage);
