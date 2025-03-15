a3c8fb346d0cdfee17d7277d51b7d337b35b8a15
vikanshu-98 <vikanshu8954985003@gmail.com>
2025-03-15 10:53:01 +0530

    fixed bulk upload

--- resources/views/verify/bulk.blade.php
+++ resources/views/verify/bulk.blade.php
@@ -3,27 +3,46 @@
 @section('main-section')
     @push('styles')
         <link rel="stylesheet" href="{{ asset('verify/bulk-upload/css/style.css') }}">
+        <link rel="shortcut icon" href="assets/bouncee-logo.png" type="image/png">
         <!-- jQuery CDN -->
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://unpkg.com/filepond/dist/filepond.min.js"></script>
+        {{-- <script src="https://unpkg.com/filepond/dist/filepond.min.js"></script>
         <script src="https://unpkg.com/jquery-filepond/filepond.jquery.js"></script>
 
-        <link rel="shortcut icon" href="assets/bouncee-logo.png" type="image/png">
-
-        <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
+         <link href="https://unpkg.com/filepond/dist/filepond.css" rel="stylesheet">
         <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet">
         <link href="https://unpkg.com/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.css" rel="stylesheet">
         <link href="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.css" rel="stylesheet">
         <link href="https://unpkg.com/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.css" rel="stylesheet">
     
 
-
-         
+       
         <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js"></script>
         <script src="https://unpkg.com/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.js"></script>
         <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js"></script>
-        <script src="https://unpkg.com/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.js"></script>
-        
+        <script src="https://unpkg.com/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.js"></script> --}}
+            
+
+        <!-- Load FilePond Core -->
+        <script src="https://unpkg.com/filepond/dist/filepond.min.js"></script>
+
+        <!-- Load FilePond Plugins -->
+        <script src="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.js"></script>
+        <script src="https://unpkg.com/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.min.js"></script>
+        <script src="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.min.js"></script>
+        <script src="https://unpkg.com/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.min.js"></script>
+
+        <!-- jQuery FilePond (only if needed) -->
+        <script src="https://unpkg.com/jquery-filepond/filepond.jquery.js"></script>
+
+        <!-- FilePond Styles -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/filepond/4.30.4/filepond.min.css" rel="stylesheet">
+        <link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet">
+        <link href="https://unpkg.com/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.css" rel="stylesheet">
+        <link href="https://unpkg.com/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.css" rel="stylesheet">
+        <link href="https://unpkg.com/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.css" rel="stylesheet">
+
+
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css"> 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     
