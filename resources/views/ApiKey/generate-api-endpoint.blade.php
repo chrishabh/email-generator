20a43dd041113f923d2a6ce8a23c109e9fb2d959
vikanshu-98 <vikanshu8954985003@gmail.com>
2025-03-20 22:46:44 +0530

    revert

--- resources/views/ApiKey/generate-api-endpoint.blade.php
+++ resources/views/ApiKey/generate-api-endpoint.blade.php
@@ -137,7 +137,7 @@
 
 
     <script src="https://cdn.jsdelivr.net/npm/materialize-css@1.0.0/dist/js/materialize.min.js"></script> 
-    {{-- <script src="https://cdn.tailwindcss.com"></script>  --}}
+    <script src="https://cdn.tailwindcss.com"></script> 
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script> 
     <script src="{{ asset('api/js/script.js') }}" type="text/javascript"></script>
 
