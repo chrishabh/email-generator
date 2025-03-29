0a173c8382db337c9da76b764072ad5546c42540
chrishabh <ch.rishabh8527@gmail.com>
2025-03-30 01:24:51 +0530

    Update

--- resources/views/verify/bulk.blade.php
+++ resources/views/verify/bulk.blade.php
@@ -84,10 +84,10 @@
                                                     @php
                                                         $statusOfVer = $v['apiStatus'];
                                                         $color='';
-                                                        if($statusOfVer =='deliverable') $color='#28A745';
-                                                        else if($statusOfVer =='undeliverable') $color='#DC3545';
-                                                        else if($statusOfVer =='unknown') $color='#FFC107';
-                                                        else if($statusOfVer =='accept all') $color='#6C757D';
+                                                        if($statusOfVer =='Deliverable') $color='#28A745';
+                                                        else if($statusOfVer =='Undeliverable') $color='#DC3545';
+                                                        else if($statusOfVer =='Unknown') $color='#FFC107';
+                                                        else if($statusOfVer =='Accept all') $color='#6C757D';
                                                     @endphp
                                                     <div class="col-md-6 stat-col pb-3">
                                                         <span class="text text-capitalize">{{$v['apiStatus']}}</span> 
