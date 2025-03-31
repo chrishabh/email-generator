51595778832f73fa8ffb8dd7b1a9612cff748cae
vikanshu-98 <vikanshu8954985003@gmail.com>
2025-04-01 00:23:03 +0530

    changes

--- resources/views/verify/single.blade.php
+++ resources/views/verify/single.blade.php
@@ -68,14 +68,20 @@
                                 @if (!empty($data))
                                     @php 
                                         $status = $data['status'];
-                                        $email  = $data['email'];
+                                        $email  = $data['email']; 
+                                        $color  = '';
+                                        pp($status);
+                                        if($status =='deliverable') {$color='#28A745';}
+                                        else if($status =='undeliverable') $color='#DC3545';
+                                        else if($status =='unknown') $color='#FFC107';
+                                        else if($status =='accept all') $color='#6C757D';
                                     @endphp
-                                    <div class="correct-email {{$status=='deliverable'?'valid-email':'invalid-email'}}">
+                                    <div class="correct-email" style="border: 2px solid {{$color}}">
                                         <div class="col-md-8"> 
                                             <h2> <i class="fa-light fa-check"></i>  {{ $email }}</h2>
                                         </div>
                                         <div class="col-md-3 col-offset-1 px-0"> 
-                                            <div class="status--div  {{$status=='deliverable' ? 'bg-success-valid' :'bg-danger-valid'}}">{{ $status }}</div>
+                                            <div class="status--div" style="background-color: {{$status}}">{{ $status }}</div>
                                         </div> 
                                     </div>  
                                     @else
@@ -107,15 +113,24 @@
                                         @php
                                             $status = $data['status'];
                                             $email = $data['email'];
+                                            $color  = ''; 
+                                            if($status =='deliverable') {$color='#28A745';}
+                                            else if($status =='undeliverable') $color='#DC3545';
+                                            else if($status =='unknown') $color='#FFC107';
+                                            else if($status =='accept all') $color='#6C757D';
                                         @endphp
-                                        <div
-                                            class="correct-email {{ $status != null && $status == 'deliverable' ? 'valid-email' : 'invalid-email' }}">
+                                        <style>
+                                            .correct-email .fa-light::before{
+                                                font-size: 14px;
+                                            }
+                                        </style>
+                                        <div class="correct-email" style="font-size:14px;color:{{$color}};border: 2px solid {{$color}}">
                                             <div class="col-md-8">
-                                                <h2> <i class="fa-light fa-check"></i> {{ $email }}</h2>
+                                                <h2 style="color:{{$color}}"> <i class="fa-light fa-check" style="color:{{$color}}"></i> {{ $email }}</h2>
                                             </div>
                                             <div class="col-md-3 col-offset-1 px-0">
                                                 <div
-                                                    class="status--div  {{ $status != null && $status == 'deliverable' ? 'bg-success-valid' : 'bg-danger-valid' }}">
+                                                    class="status--div"  style="background-color: {{$color}}">
                                                     {{ $status }}</div>
                                             </div>
                                         </div>
