7f7f87f71ea154329ae8c984572b5a3389108f40
vikanshu-98 <vikanshu8954985003@gmail.com>
2025-04-11 23:51:00 +0530

    api route

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,9 @@ try{
     Route::get('/bouncee-verification/{hash}', function() {})->middleware('user.email.verification');
     Route::middleware(['guest','session.timeout'])->group(function(){
 
-        // Route::get('/', function () {
-        //     return view('index');
-        // });
+        Route::get('/', function () {
+            return view('index');
+        });
       
         Route::get('/signup',[RegisterController::class,'showRegistrationForm'])->name('signup');
         Route::post('/signup',[RegisterController::class,'signup']); 
