d8f5544beedfd49a3782a82edfa42d07c222ab77
vikanshu-98 <vikanshu8954985003@gmail.com>
2025-08-08 23:30:21 +0530

    fixes

--- public/integration/js/index.js
+++ public/integration/js/index.js
@@ -75,7 +75,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 if(integration.tool.name.toLowerCase() === 'web engage'){ 
                     OpenModalImport = `ImportData('${integration.tool.name}','${integration.mc_user_id}','${integration.mc_dc}','${integration.id}','webengage')`
                 }else{
-                   OpenModalImport =  `openModalForImport('${integration.tool.name}','${integration.tool.icon_url}','${integration.name}','${integration.mc_user_id}','${integration.mc_dc}','${integration.id}')`
+                    const safeName     = integration.name.replace(/'/g, "\\'"); 
+                   OpenModalImport =  `openModalForImport('${integration.tool.name}','${integration.tool.icon_url}','${safeName}','${integration.mc_user_id}','${integration.mc_dc}','${integration.id}')`
                 }
                 const dropdownId = `dropdown-${integration.id}`;
                 card.className = 'bg-gray-100 px-3 py-3 rounded shadow mb-4';
