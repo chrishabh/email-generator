2692f031138a51a5ae1be8a91dd83815649371f1
chrishabh <chrishabh>
2025-10-28 22:22:26 +0530

    uPDATE

--- config/services.php
+++ config/services.php
@@ -31,9 +31,9 @@ return [
     ],
 
     'google' => [
-        'client_id'     => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect'      => env('GOOGLE_REDIRECT_URI'),
+        'client_id'     => env('AUTH_GOOGLE_CLIENT_ID'),
+        'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('AUTH_GOOGLE_REDIRECT_URI'),
     ],
 
 ];
